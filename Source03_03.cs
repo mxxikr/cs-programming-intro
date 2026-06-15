69f3b76e4bb18f81c2b0c1537cfa610d6a193264
mxxikr <mxxikr@gmail.com>
2026-06-15 20:13:14 +0900

    숫자 1부터 10까지 출력하는 while문

--- Source03_03.cs
+++ Source03_03.cs
@@ -1,12 +1,12 @@
-﻿namespace Variable;
-
-using System;
-
-class MainClass {
-    static void Main(string[] args)
-    {
-
-        for (int i = 1; i < 11; i++)
-            Console.WriteLine();
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class MainClass {
+//     static void Main(string[] args)
+//     {
+//
+//         for (int i = 1; i < 11; i++)
+//             Console.WriteLine();
+//     }
+// }
---
+++ Source03_04.cs
@@ -0,0 +1,13 @@
+﻿namespace Variable;
+
+using System;
+
+class MainClass {
+    static void Main(string[] args)
+    {
+        int i = 1;
+        
+        while (i < 11)
+            Console.WriteLine(i++);
+    }
+}
