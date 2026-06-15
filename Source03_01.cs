d0ff826ef423d0f119abc0efae64df689cdafb3a
mxxikr <mxxikr@gmail.com>
2026-06-15 20:04:38 +0900

    양수와 음수, 0 구분하기

--- Source03_01.cs
+++ Source03_01.cs
@@ -1,19 +1,19 @@
-﻿namespace Variable;
-
-class MainClass
-{
-    static void Main(string[] args)
-    {
-        // 변수 선언과 초기화
-        bool boolVariable = true;
-        int intVariable = 10;
-        float floatVariable = 3.4f;
-        char charVariable = 'a';
-        
-        // 변수에 저장 된 값 콘솔에 출력
-        Console.WriteLine(boolVariable);
-        Console.WriteLine(intVariable);
-        Console.WriteLine(floatVariable);
-        Console.WriteLine(charVariable);
-    }
-}
+﻿// namespace Variable;
+//
+// class MainClass
+// {
+//     static void Main(string[] args)
+//     {
+//         // 변수 선언과 초기화
+//         bool boolVariable = true;
+//         int intVariable = 10;
+//         float floatVariable = 3.4f;
+//         char charVariable = 'a';
+//         
+//         // 변수에 저장 된 값 콘솔에 출력
+//         Console.WriteLine(boolVariable);
+//         Console.WriteLine(intVariable);
+//         Console.WriteLine(floatVariable);
+//         Console.WriteLine(charVariable);
+//     }
+// }
---
+++ Source03_02.cs
@@ -0,0 +1,17 @@
+﻿namespace Variable;
+
+using System;
+
+class MainClass {
+    static void Main(string[] args)
+    {
+        int num = 0;
+        
+        if (num > 0)
+            Console.WriteLine("양수");
+        else if (num < 0)
+            Console.WriteLine("음수");
+        else 
+            Console.WriteLine("영");
+    }
+}
