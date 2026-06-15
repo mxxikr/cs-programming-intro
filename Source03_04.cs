506d5c8b70218029f6e8dc1f3df9bfb836f5e12f
mxxikr <mxxikr@gmail.com>
2026-06-15 20:15:59 +0900

    산술 연산자 실습

--- Source03_04.cs
+++ Source03_04.cs
@@ -1,13 +1,13 @@
-﻿namespace Variable;
-
-using System;
-
-class MainClass {
-    static void Main(string[] args)
-    {
-        int i = 1;
-        
-        while (i < 11)
-            Console.WriteLine(i++);
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class MainClass {
+//     static void Main(string[] args)
+//     {
+//         int i = 1;
+//         
+//         while (i < 11)
+//             Console.WriteLine(i++);
+//     }
+// }
---
+++ Source03_05.cs
@@ -0,0 +1,27 @@
+﻿namespace Variable;
+
+using System;
+
+class MainClass {
+    static void Main(string[] args)
+    {
+        int result, num1, num2;
+
+        result = 3 + 1;
+        Console.WriteLine(result);
+        
+        result = 3 - 1;
+        Console.WriteLine(result);
+
+        num1 = 2;
+        result = 5 * num1;
+        Console.WriteLine(result);
+
+        num2 = 10;
+        result = num2 / 3;
+        Console.WriteLine(result);
+        
+        result = 10 % 2;
+        Console.WriteLine(result);
+    }
+}
