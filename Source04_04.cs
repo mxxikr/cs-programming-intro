d42caf43fc16bc70acd734e903c8c1aa1a0a7343
mxxikr <mxxikr@gmail.com>
2026-06-15 20:42:23 +0900

    오버로딩 구현하기

---
+++ Source04_04.cs
@@ -0,0 +1,31 @@
+﻿namespace Variable;
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+    public int Weight = 0;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 " + Name + "입니다.");
+    }
+
+    public Cat(String name, int weight)
+    {
+        Name = name;
+        Weight = weight;
+        Console.WriteLine("고양이의 이름은 " + Name +  "이며, 몸무게는 " + Weight + "kg입니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat coco = new Cat("코코");
+        Cat moly = new Cat("몰리", 3);
+    }
+}
