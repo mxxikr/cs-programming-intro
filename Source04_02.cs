b036477e5ac5e27defb55a6aa6a4de0d3e4f7227
mxxikr <mxxikr@gmail.com>
2026-06-15 20:35:42 +0900

    클래스의 생성자 만들기

---
+++ Source04_02.cs
@@ -0,0 +1,21 @@
+﻿namespace Variable;
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat()
+    {
+        Console.WriteLine("생성자가 호출되었습니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat myCat = new Cat();
+    }
+}
