49e250ff86fcf443ef47bbca74aa30c7f2a097e1
mxxikr <mxxikr@gmail.com>
2026-06-15 20:32:05 +0900

    인스턴스 생성하기

--- Source03_07.cs
+++ Source03_07.cs
@@ -1,33 +1,33 @@
-﻿namespace Variable;
-
-using System;
-
-class MainClass {
-    static void Main(string[] args)
-    {
-        bool result;
-        int num1, num2;
-
-        num1 = 3;
-        num2 = 5;
-        
-        result = num1 > num2;
-        Console.WriteLine(result);
-        
-        result = num1 < num2;
-        Console.WriteLine(result);
-        
-        result = num1 >= num2;
-        Console.WriteLine(result);
-        
-        result = num1 <= num2;
-        Console.WriteLine(result);
-        
-        result = num1 == num2;
-        Console.WriteLine(result);
-        
-        result = num1 != num2;
-        Console.WriteLine(result);
-        
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class MainClass {
+//     static void Main(string[] args)
+//     {
+//         bool result;
+//         int num1, num2;
+//
+//         num1 = 3;
+//         num2 = 5;
+//         
+//         result = num1 > num2;
+//         Console.WriteLine(result);
+//         
+//         result = num1 < num2;
+//         Console.WriteLine(result);
+//         
+//         result = num1 >= num2;
+//         Console.WriteLine(result);
+//         
+//         result = num1 <= num2;
+//         Console.WriteLine(result);
+//         
+//         result = num1 == num2;
+//         Console.WriteLine(result);
+//         
+//         result = num1 != num2;
+//         Console.WriteLine(result);
+//         
+//     }
+// }
---
+++ Source04_01.cs
@@ -0,0 +1,37 @@
+﻿namespace Variable;
+
+using System;
+
+class Person
+{
+
+    public string Name = null;
+    public string Birthday = null;
+    public string Gender = null;
+
+    public void Eat()
+    {
+        Console.WriteLine(Name + "이(가) 아침을 먹습니다.");
+    }
+
+    public void Walk()
+    {
+        Console.WriteLine(Name + "이(가) 걷습니다.");
+    }
+    
+    public void Run()
+    {
+        Console.WriteLine(Name + "이(가) 뜁니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Person p1;
+        p1 = new Person();
+        p1.Name = "서준";
+        p1.Eat();
+    }
+}
