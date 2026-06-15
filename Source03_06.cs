a971062df6c8b42423426bfa26a76857ffc95990
mxxikr <mxxikr@gmail.com>
2026-06-15 20:20:49 +0900

    관계 연산자 실습

--- Source03_06.cs
+++ Source03_06.cs
@@ -1,14 +1,14 @@
-﻿namespace Variable;
-
-using System;
-
-class MainClass {
-    static void Main(string[] args)
-    {
-        int num = 0;
-        Console.WriteLine(num++);
-        Console.WriteLine(num);
-        Console.WriteLine(--num);
-        Console.WriteLine(num);
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class MainClass {
+//     static void Main(string[] args)
+//     {
+//         int num = 0;
+//         Console.WriteLine(num++);
+//         Console.WriteLine(num);
+//         Console.WriteLine(--num);
+//         Console.WriteLine(num);
+//     }
+// }
