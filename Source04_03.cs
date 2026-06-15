6b9049c01f40aea8981126ce510d882e62fbfec1
mxxikr <mxxikr@gmail.com>
2026-06-15 20:38:04 +0900

    생성자로 초기화하기

---
+++ Source04_03.cs
@@ -0,0 +1,23 @@
+﻿namespace Variable;
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 " + Name + "입니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat coco = new Cat("코코");
+        Cat moly = new Cat("몰리");
+    }
+}
