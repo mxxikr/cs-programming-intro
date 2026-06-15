32063cee9affc5985eee9bc633f0fee535eaa641
mxxikr <mxxikr@gmail.com>
2026-06-15 20:51:34 +0900

    메서드 오버라이딩 구현하기

--- Source04_06.cs
+++ Source04_06.cs
@@ -1,29 +1,29 @@
-﻿namespace Variable;
-
-using System;
-
-class Robot
-{
-    public void Move()
-    {
-        Console.WriteLine("Robot is moving");
-    }
-}
-
-class CleanRobot : Robot
-{
-    public void Clean()
-    {
-        Console.WriteLine("Robot is cleaning");
-    }
-}
-
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        CleanRobot cleanRobot = new CleanRobot();
-        cleanRobot.Move();
-        cleanRobot.Clean();
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class Robot
+// {
+//     public void Move()
+//     {
+//         Console.WriteLine("Robot is moving");
+//     }
+// }
+//
+// class CleanRobot : Robot
+// {
+//     public void Clean()
+//     {
+//         Console.WriteLine("Robot is cleaning");
+//     }
+// }
+//
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         CleanRobot cleanRobot = new CleanRobot();
+//         cleanRobot.Move();
+//         cleanRobot.Clean();
+//     }
+// }
