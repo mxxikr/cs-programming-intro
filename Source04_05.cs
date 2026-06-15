bbef066c249cf613cc665557e084027410d360ca
mxxikr <mxxikr@gmail.com>
2026-06-15 20:49:14 +0900

    클래스 상속하기

--- Source04_05.cs
+++ Source04_05.cs
@@ -1,29 +1,29 @@
-﻿namespace Variable;
-
-using System;
-
-class Cat
-{
-    public string Name = null;
-
-    public Cat(string name)
-    {
-        Name = name;
-        Console.WriteLine("고양이의 이름은 " + Name + "입니다.");
-    }
-
-    ~Cat()
-    {
-        Console.WriteLine(Name + "가 사라집니다.");
-    }
-    
-}
-
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        Cat coco = new Cat("코코");
-        Cat moly = new Cat("몰리");
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class Cat
+// {
+//     public string Name = null;
+//
+//     public Cat(string name)
+//     {
+//         Name = name;
+//         Console.WriteLine("고양이의 이름은 " + Name + "입니다.");
+//     }
+//
+//     ~Cat()
+//     {
+//         Console.WriteLine(Name + "가 사라집니다.");
+//     }
+//     
+// }
+//
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         Cat coco = new Cat("코코");
+//         Cat moly = new Cat("몰리");
+//     }
+// }
