5d2a29048d4841588f911a5f896d5853a8005a1b
mxxikr <mxxikr@gmail.com>
2026-06-15 20:11:18 +0900

    숫자 1부터 10까지 출력하는 for문

--- Source03_02.cs
+++ Source03_02.cs
@@ -1,17 +1,17 @@
-﻿namespace Variable;
-
-using System;
-
-class MainClass {
-    static void Main(string[] args)
-    {
-        int num = 0;
-        
-        if (num > 0)
-            Console.WriteLine("양수");
-        else if (num < 0)
-            Console.WriteLine("음수");
-        else 
-            Console.WriteLine("영");
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class MainClass {
+//     static void Main(string[] args)
+//     {
+//         int num = 0;
+//         
+//         if (num > 0)
+//             Console.WriteLine("양수");
+//         else if (num < 0)
+//             Console.WriteLine("음수");
+//         else 
+//             Console.WriteLine("영");
+//     }
+// }
