71913b34f18bdc330211a3dc51bb3ba1db00fa8d
mxxikr <mxxikr@gmail.com>
2026-06-16 20:45:18 +0900

    indexOf 메서드 사용하기

---
+++ Source05_06.cs
@@ -0,0 +1,11 @@
+namespace Variable;
+
+class MainClass {
+  public static void Main(string[] args)
+  {
+    int[] array = { 1, 2, 3, 4, 5 };
+    
+    // 배열에서 3이 위치한 인덱스 출력
+    Console.WriteLine(Array.IndexOf(array, 3));
+  }
+}
