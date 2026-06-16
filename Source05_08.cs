3e54e313c7da768af4ad668606dfbeb5887c58b7
mxxikr <mxxikr@gmail.com>
2026-06-16 20:59:29 +0900

    Stack 클래스 사용하기

--- Source05_08.cs
+++ Source05_08.cs
@@ -1,21 +1,21 @@
-using System.Collections;
-
-namespace Variable;
-
-class MainClass {
-  public static void Main(string[] args)
-  {
-    Queue qu = new Queue();
-    
-    // Enqueue() 메서드로 큐에 아이템 추가
-    qu.Enqueue(1);
-    qu.Enqueue(2);
-    qu.Enqueue(3);
-    
-    // Dequeue() 메서드로 큐에서 아이템 제거
-    while (qu.Count > 0)
-    {
-      Console.WriteLine(qu.Dequeue());
-    }
-  }
-}
+// using System.Collections;
+//
+// namespace Variable;
+//
+// class MainClass {
+//   public static void Main(string[] args)
+//   {
+//     Queue qu = new Queue();
+//     
+//     // Enqueue() 메서드로 큐에 아이템 추가
+//     qu.Enqueue(1);
+//     qu.Enqueue(2);
+//     qu.Enqueue(3);
+//     
+//     // Dequeue() 메서드로 큐에서 아이템 제거
+//     while (qu.Count > 0)
+//     {
+//       Console.WriteLine(qu.Dequeue());
+//     }
+//   }
+// }
