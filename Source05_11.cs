c895fe17e2eae650e55317c1e19016f89b600d99
mxxikr <mxxikr@gmail.com>
2026-06-16 21:09:02 +0900

    try ~ catch문으로 예외 처리하기

--- Source05_11.cs
+++ Source05_11.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-
-namespace Variable;
-
-class MainClass {
-  public static void Main(string[] args)
-  { 
-    Console.WriteLine("나눌 숫자를 입력하세요 : ");
-    int divider = int.Parse(Console.ReadLine());
-    Console.WriteLine(10 / divider);
-  }
-}
+// using System.Collections;
+//
+// namespace Variable;
+//
+// class MainClass {
+//   public static void Main(string[] args)
+//   { 
+//     Console.WriteLine("나눌 숫자를 입력하세요 : ");
+//     int divider = int.Parse(Console.ReadLine());
+//     Console.WriteLine(10 / divider);
+//   }
+// }
---
+++ Source05_12.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+
+namespace Variable;
+
+class MainClass {
+  public static void Main(string[] args)
+  { 
+    Console.WriteLine("나눌 숫자를 입력하세요 : ");
+    int divider = int.Parse(Console.ReadLine());
+
+    try
+    {
+      Console.WriteLine(10 / divider);
+    }
+    catch
+    {
+      Console.WriteLine("0으로 나눌 수 없습니다.");
+    }
+  }
+}
