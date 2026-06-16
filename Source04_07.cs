653165cb20fc6ab68e911fd8db89f168691dbd73
mxxikr <mxxikr@gmail.com>
2026-06-16 19:59:18 +0900

    private, this 키워드 사용

--- Source04_07.cs
+++ Source04_07.cs
@@ -1,38 +1,38 @@
-﻿namespace Variable;
-
-using System;
-
-class Robot
-{
-    public void Move()
-    {
-        Console.WriteLine("Robot is moving");
-    }
-}
-
-class CleanRobot : Robot
-{
-    public void Move()
-    {
-        Console.WriteLine("CleanRobot is moving");
-    }
-}
-
-class RescueRobot : Robot
-{
-    public void Move()
-    {
-        Console.WriteLine("RescueRobot is moving");
-    }
-}
-
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        CleanRobot cleanRobot = new CleanRobot();
-        RescueRobot rescueRobot = new RescueRobot();
-        cleanRobot.Move();
-        rescueRobot.Move();
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class Robot
+// {
+//     public void Move()
+//     {
+//         Console.WriteLine("Robot is moving");
+//     }
+// }
+//
+// class CleanRobot : Robot
+// {
+//     public void Move()
+//     {
+//         Console.WriteLine("CleanRobot is moving");
+//     }
+// }
+//
+// class RescueRobot : Robot
+// {
+//     public void Move()
+//     {
+//         Console.WriteLine("RescueRobot is moving");
+//     }
+// }
+//
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         CleanRobot cleanRobot = new CleanRobot();
+//         RescueRobot rescueRobot = new RescueRobot();
+//         cleanRobot.Move();
+//         rescueRobot.Move();
+//     }
+// }
