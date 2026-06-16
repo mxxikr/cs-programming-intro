fe85c0cd8278e72b8f07b8885ba1d4546ff2e6a7
mxxikr <mxxikr@gmail.com>
2026-06-16 20:07:36 +0900

    자동차 추상화하기

--- Source04_08.cs
+++ Source04_08.cs
@@ -1,29 +1,29 @@
-﻿namespace Variable;
-
-using System;
-
-class Cat
-{
-    private string name = null;
-
-    public void SetName(string name)
-    {
-        this.name = name;
-    }
-
-    public string GetName()
-    {
-        return this.name;
-    }
-
-}
-
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        Cat coco = new Cat();
-        coco.SetName("Coco");
-        Console.WriteLine(coco.GetName());
-    }
-}
+﻿// namespace Variable;
+//
+// using System;
+//
+// class Cat
+// {
+//     private string name = null;
+//
+//     public void SetName(string name)
+//     {
+//         this.name = name;
+//     }
+//
+//     public string GetName()
+//     {
+//         return this.name;
+//     }
+//
+// }
+//
+// class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         Cat coco = new Cat();
+//         coco.SetName("Coco");
+//         Console.WriteLine(coco.GetName());
+//     }
+// }
