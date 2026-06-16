ccba5146e8f1e82b27633316507f15bbc2e7acc1
mxxikr <mxxikr@gmail.com>
2026-06-16 20:36:12 +0900

    반복문으로 배열의 값 출력하기

---
+++ Source05_03.cs
@@ -0,0 +1,17 @@
+namespace Variable;
+
+class MainClass {
+  public static void Main(string[] args)
+  {
+    
+    // 배열을 초기화하는 두 번째 방법
+    int[] array1 = new int[] { 1, 2, 3 };
+    
+    
+    // for 반복문으로 배열의 인덱스 접근과 값 출력
+    for (int i = 0; i < 3; i++)
+    {
+      Console.WriteLine(array1[i]);
+    }
+  }
+}
