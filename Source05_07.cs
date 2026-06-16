f506acb0d4b0f4dc826a5d2591d4fb78c41a9de6
mxxikr <mxxikr@gmail.com>
2026-06-16 20:53:58 +0900

    ArrayList 클래스 사용하기

---
+++ Source05_07.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+
+namespace Variable;
+
+class MainClass {
+  public static void Main(string[] args)
+  {
+    ArrayList al = new ArrayList();
+    
+    // Add() 메서드로 ArrayList에 아이템 추가
+    al.Add(1);
+    al.Add("Hello");
+    al.Add(3.3);
+    al.Add(true);
+
+    foreach (var item in al)
+    {
+      Console.WriteLine(item);
+    }
+
+    Console.WriteLine();
+    
+    // Remove() 메서드로 ArrayList에서 아이템 삭제
+    al.Remove("Hello");
+
+    foreach (var item in al)
+    {
+      Console.WriteLine(item);
+    }
+  }
+}
